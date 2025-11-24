3027059cb925953cfa210f7bb66d39e0e91f544f
Prokofev Maksim <abobuxs.123@gmail.com>
2025-11-24 21:27:55 +0300

    Connecte all moduls in main app

--- memory_operations.py
+++ memory_operations.py
@@ -3,23 +3,18 @@ class Memory:
         self._memory = 0
     
     def memory_add(self, value):
-        """M+ - добавить значение к памяти"""
         self._memory += float(value)
     
     def memory_subtract(self, value):
-        """M- - вычесть значение из памяти"""
         self._memory -= float(value)
     
     def memory_recall(self):
-        """MR - получить значение из памяти"""
         return self._memory
     
     def memory_clear(self):
-        """MC - очистить память"""
         self._memory = 0
     
     def memory_store(self, value):
-        """MS - сохранить значение в память"""
         self._memory = float(value)
     
     def get_memory_value(self):
